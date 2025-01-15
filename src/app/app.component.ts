f7dd0e996b2bdd08a2b96770a964563ecb980bbb
DomCamillo <159430752+DomCamillo@users.noreply.github.com>
2025-01-15 16:12:13 +0100

    added material design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { StartScreenComponent } from "./start-screen/start-screen.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StartScreenComponent],
+  imports: [CommonModule, RouterOutlet, StartScreenComponent, ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ringoffire';
 }
+
+
+
+
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent,MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
-export class GameComponent {
+
+
+
+export class GameComponent implements OnInit  {
+  pickAnimationCard = false;
+  currentCard? = '';
+  game: Game;
+
+  constructor(){
+    this.game = new Game()
+    
+  }
+
+  ngOnInit(): void{
+    this.newGame()
+  }
+
+  newGame(){
+    
+    console.log(this.game);
+    
+  }
+
+  takeCard(){
+    if (!this.pickAnimationCard) {
+      
+   this.currentCard = this.game.stack.pop()
+   this.pickAnimationCard = true;
+   console.log(this.currentCard);
+
+  
+   setTimeout(() => {
+    this.pickAnimationCard = false;
+    if (this.currentCard) {
+      this.game.palyedCards.push(this.currentCard);
+      console.log('new card',this.game);
+                                                     //this.game.palyedCards.push(this.currentCard!);
+     } else {
+      console.error('currentCard is undefined');
+     }
+   }, 500);
+  }
+}
+
+
 
 }
+
+
